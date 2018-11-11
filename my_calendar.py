fc20c7519cfe844a4141acb4ef796a08e1c8587d
Eloy <eloyparra_b@hotmail.com>
2018-11-12 00:16:36 +0100

    everything can be plotted, messy

--- my_calendar.py
+++ my_calendar.py
@@ -11,15 +11,10 @@ from subprocess import call
 from scipy.stats.stats import pearsonr
 from datetime import date, datetime, timedelta
 
-
-def get_value(field, cast_type):
-    while True:
-        try:
-            value = cast_type(input(field + ': '))
-            break
-        except ValueError:
-            print("Expecting " + str(cast_type))
-    return value
+# TO DO
+# plot all results of correlation in multiple figures if necessary
+# correlate all things
+# ignore days
 
 
 class Calendar:
@@ -40,14 +35,14 @@ class Calendar:
 
         self.path = lines[0]
         self.fields = []
-        self.types = []
+        self.types = {}
         self.implicit_sources = {}
         self.implicit_keywords = {}
 
         for line in lines[1:]:
             words = line.split()
-            self.types.append(locate(words[0]))
             self.fields.append(words[1])
+            self.types[words[1]] = locate(words[0])
             if len(words) > 2:
                 self.implicit_sources[words[1]] = words[2].split(':')[0]
                 self.implicit_keywords[words[1]] = words[2].split(':')[1]
@@ -81,9 +76,26 @@ class Calendar:
         self.database = new_database
         self.dump()
 
+    def fill_fields(self, day):
+        for field, field_type in zip(self.fields, self.types):
+            if field not in self.implicit_keywords:
+                while True:
+                    value = input(field + ': ')
+                    if value != '':
+                        try:
+                            day[field] = field_type(value)
+                            break
+                        except ValueError:
+                            print("Expecting " + str(field_type))
+                    else:
+                        break
+
     def fill_implicit_fields(self, day):
         for field, keyword in self.implicit_keywords.items():
-            day[field] = keyword in day[self.implicit_sources[field]]
+            if self.implicit_sources[field] in day:
+                day[field] = keyword in day[self.implicit_sources[field]]
+            else:
+                day[field] = False
 
     @staticmethod
     def visual_aid():
@@ -113,14 +125,11 @@ class Calendar:
             processing_date = last_date + timedelta(days=i)
             print("Data for", calendar.day_name[processing_date.weekday()], processing_date, "\n")
             # if any of the fields expects a number, print a scale from 0 to 10 as a visual aid
-            if int in self.types or float in self.types:
+            if int in self.types.values or float in self.types.values:
                 print(self.visual_aid())
 
             day = {'date': str(processing_date)}
-            for field, field_type in zip(self.fields, self.types):
-                if field not in self.implicit_keywords:
-                    value = get_value(field, field_type)
-                    day[field] = value
+            self.fill_fields(day)
             self.fill_implicit_fields(day)
             self.database.append(day)
 
@@ -149,7 +158,7 @@ class Calendar:
         day = json.dumps(self.database[edit_index], indent=4, separators=(',', ': '), ensure_ascii=False)
 
         with tempfile.NamedTemporaryFile(suffix=".tmp") as tf:
-            if int in self.types or float in self.types:
+            if int in self.types.values or float in self.types.values:
                 initial_string = self.visual_aid() + '\n'
                 tf.write(bytes(initial_string, 'utf-8'))
             tf.write(bytes(day, 'utf-8'))
@@ -218,11 +227,87 @@ class Calendar:
 
         print("\n")
 
+    def read_values(self, field, first_index, last_index, precision=1):
+        mask = np.zeros(last_index-first_index)
+        values = np.zeros(last_index-first_index)
+        for day_num, day_index in enumerate(range(first_index, last_index)):
+            day = self.database[day_index]
+            if field in day:
+                values[day_num] = round(day[field], precision)
+            else:
+                mask[day_num] = 1
+        return np.array(values), np.array(mask)
+
+    def list_to_bool(self, field, first_index, last_index, separator=', '):
+        # get the list of elements and how many times each of them appears
+        unsorted_list = []
+        unsorted_counts = []
+        for day_num, day_index in enumerate(range(first_index, last_index)):
+            day = self.database[day_index]
+            if field in day:
+                for elem in day[field].split(separator):
+                    if elem in unsorted_list:
+                        unsorted_counts[unsorted_list.index(elem)] += 1
+                    else:
+                        unsorted_list.append(elem)
+                        unsorted_counts.append(1)
+
+        # sort factors by frequency
+        sorted_counts = sorted(unsorted_counts, reverse=True)
+        sorted_list = sorted(unsorted_list, reverse=True,
+                             key=lambda e: unsorted_counts[unsorted_list.index(e)])
+
+        # print sorted list
+        for factor, count in zip(sorted_list, sorted_counts):
+            print(count, factor)
+        print('\n')
+
+        # create matrix
+        mat = np.zeros((last_index-first_index, len(sorted_list)))
+        mask = np.zeros(last_index-first_index)
+        for day_num, day_index in enumerate(range(first_index, last_index)):
+            day = self.database[day_index]
+            if field in day:
+                day_list = day[field].split(separator)
+                for elem_num, elem in enumerate(sorted_list):
+                    if elem in day_list:
+                        mat[day_num, elem_num] = 1
+            else:
+                mask[day_num] = 1
+        return mat, mask, sorted_list
+
+    @staticmethod
+    def bool_to_intervals(values, mask):
+        indices = []
+        intervals = []
+        intervals_mask = []
+        found_stream = False
+        for day_num, value in enumerate(values):
+            if value and not found_stream:
+                found_stream = True
+                beginning = day_num
+            elif found_stream and mask[day_num]:
+                found_stream = False
+                indices.append(day_num)
+                intervals.append(0)
+                intervals_mask.append(1)
+            elif found_stream and value:
+                indices.append(day_num)
+                intervals.append(day_num - beginning)
+                intervals_mask.append(0)
+                beginning = day_num
+        return indices, intervals, intervals_mask
+
+    @staticmethod
+    def moving_average(day_indices, values, mask, window):
+        pass
+
     def plot(self):
         parser = argparse.ArgumentParser()
         parser.add_argument("fields", help="fields to plot separated by commas (without spaces)")
         parser.add_argument("num_days", type=int, help="number of days to plot")
         parser.add_argument("-d", "--date", help="first date to plot")
+        parser.add_argument("-a", "--average", type=int, help="width of the moving average in days")
         args = parser.parse_args()
 
         first_index, last_index = self.index_range(args.num_days, args.date)
@@ -233,113 +318,96 @@ class Calendar:
         dates = [first_date + timedelta(days=n) for n in range(num_days)]
 
         fig, ax = plt.subplots()
-        ax.set_ylim(0, 10)
-        ax.set_yticks([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        ax.set_ylim(0, 1)
+        ax.set_yticks([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
         ax.yaxis.grid()
         prop_cycle = plt.rcParams['axes.prop_cycle']
         colors = prop_cycle.by_key()['color']
 
+        if '.i' in args.fields:
+            axr = ax.twinx()
+
         fields = args.fields.split(',')
         for plot_num, field in enumerate(fields):
-            field_num = self.fields.index(field)
             color = colors[plot_num % len(colors)]
-            if self.types[field_num] is bool:
+
+            got_values = False
+            is_bool = False
+            for field_name in self.fields:
+                if field_name in field and self.types[field_name] is str:
+                    mat, mask, sorted_list = self.list_to_bool(field_name, first_index, last_index)
+                    factor = field.split(':')[1].strip('.i').strip('.b')
+                    column = sorted_list.index(factor)
+                    values = mat[:, column]
+                    got_values = True
+                    is_bool = True
+            if not got_values:
+                stripped_field = field.strip('.i').strip('.b')
+                values, mask = self.read_values(stripped_field, first_index, last_index)
+
+            if '.i' in field:
+                indices, values, mask = self.bool_to_intervals(values, mask)
+                x = [dates[index] for index in indices]
+                axr.plot(x, np.ma.masked_array(values, mask), '.-', label=field, color=color)
+                axr.legend(loc='upper right')
+            elif '.b' in field:
+                ax.plot(dates, np.ma.masked_array(values, mask), '.-', label=field, color=color)
+                ax.legend(loc='upper left')
+            elif self.types[field.split(':')[0]] in (int, float):
+                values = values/10
+                ax.plot(dates, np.ma.masked_array(values, mask), '.-', label=field, color=color)
+                ax.legend(loc='upper left')
+            elif self.types[field.split(':')[0]] is bool or is_bool:
                 events = []
-                for day_num, day_index in enumerate(range(first_index, last_index)):
-                    if field in self.database[day_index] and self.database[day_index][field] == True:
+                for day_num, value in enumerate(values):
+                    if value:
+                        day_index = day_num + first_index
                         events.append(datetime.strptime(self.database[day_index]['date'], "%Y-%m-%d").date())
                 ax.eventplot(events, linelengths=20, label=field, color=color)
+                ax.legend(loc='upper left')
 
-            elif self.types[field_num] in (int, float):
-                values = np.zeros(num_days)
-                for day_num, day_index in enumerate(range(first_index, last_index)):
-                    if field in self.database[day_index]:
-                        values[day_num] = self.database[day_index][field]
-                    else:
-                        values[day_num] = -1
-                masked_values = np.ma.masked_where(values < 0, values)
-                ax.plot(dates, masked_values, '.-', label=field, color=color)
-        ax.legend()
         plt.show()
 
     def correlate(self):
         np.seterr(all='ignore')
-
         parser = argparse.ArgumentParser()
         parser.add_argument("factors_field", help="field containing lists of factors")
         parser.add_argument("values_field", help="field with values to correlate")
         parser.add_argument("-w", "--window", help="window size", default=5, type=int)
         args = parser.parse_args()
 
-        # find indices of the last uninterrupted stream of data
-        found_stream = False
-        for day_index in range(-1, -len(self.database)-1, -1):
-            if not found_stream:
-                if args.factors_field in self.database[day_index] and args.values_field in self.database[day_index]:
-                    last_index = day_index
-                    found_stream = True
-            else:
-                if (day_index == -len(self.database) or args.factors_field not in self.database[day_index]
-                        or args.values_field not in self.database[day_index]):
-                    first_index = day_index + 1
-                    break
-
-        # get the list of factors and how many times each of them appears
-        unsorted_factor_list = []
-        unsorted_factor_counts = []
-        for day_index in range(first_index, last_index+1, 1):
-            day = self.database[day_index]
-            for factor in day[args.factors_field].split(', '):
-                if factor in unsorted_factor_list:
-                    unsorted_factor_counts[unsorted_factor_list.index(factor)] += 1
-                else:
-                    unsorted_factor_list.append(factor)
-                    unsorted_factor_counts.append(1)
-
-        num_days = -(first_index - last_index - 1)
-        print('\nAnalysis based on %d days\n' % num_days)
-
-        # sort factors by frequency
-        factor_counts = sorted(unsorted_factor_counts, reverse=True)
-        factor_list = sorted(unsorted_factor_list, reverse=True, key=lambda f: unsorted_factor_counts[unsorted_factor_list.index(f)])
-
-        # print sorted list
-        for factor, count in zip(factor_list, factor_counts):
-            print(count, factor)
-        print('\n')
-
-        # create matrix
-        num_factors = len(factor_list)
-        mat = np.zeros((num_days, num_factors))
-        values = np.zeros(num_days)
-
-        for day_num, day_index in enumerate(range(first_index, last_index+1, 1)):
-            day = self.database[day_index]
-            values[day_num] = day[args.values_field]
-
-            for factor_num, factor in enumerate(factor_list):
-                if factor in day[args.factors_field].split(", "):
-                    mat[day_num, factor_num] = 1
+        factors_mat, factors_mask, factors_list = self.list_to_bool(args.factors_field, 0, len(self.database))
+        values, values_mask = self.read_values(args.values_field, 0, len(self.database))
 
         # calculate correlations
+        num_factors = len(factors_list)
         corr = np.zeros((num_factors, args.window))
 
-        for factor_num in range(num_factors):
-            for time_shift in range(0, args.window):
-                corr[factor_num, time_shift] = pearsonr(mat[0:num_days - time_shift, factor_num], values[time_shift:])[0]
+        for time_shift in range(0, args.window):
+            corrected_mat = []
+            corrected_values = []
+            for day_num, mask in enumerate(factors_mask):
+                if mask == 0:
+                    values_day_num = day_num + time_shift
+                    if values_day_num < len(values) and values_mask[values_day_num] == 0:
+                        corrected_mat.append(factors_mat[day_num])
+                        corrected_values.append(values[values_day_num])
+            corrected_mat = np.array(corrected_mat)
+            for factor_num in range(num_factors):
+                corr[factor_num, time_shift] = pearsonr(corrected_mat[:, factor_num], corrected_values)[0]
 
         # plot
         num_rows = 25
         num_cols = min(math.ceil(num_factors / num_rows), 5)
-        sharp_corr = np.pad(corr, ((0, max(num_rows * num_cols - num_factors, 0)), (0, 0)), 'constant',
+        corr = np.pad(corr, ((0, max(num_rows * num_cols - num_factors, 0)), (0, 0)), 'constant',
                             constant_values=np.nan)
 
         fig, ax = plt.subplots(1, num_cols)
         for col_num in range(num_cols):
             first_index = col_num * num_rows
             last_index = (col_num + 1) * num_rows
-            ax[col_num].matshow(sharp_corr[first_index:last_index], vmin=-1, vmax=1)
+            ax[col_num].matshow(corr[first_index:last_index], vmin=-1, vmax=1)
             ax[col_num].set_yticks([i for i in range(num_rows)])
-            ax[col_num].set_yticklabels(factor_list[first_index:last_index])
+            ax[col_num].set_yticklabels(factors_list[first_index:last_index])
 
         plt.show()
