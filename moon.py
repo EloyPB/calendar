4de01dc51b6f757e821cdb9ab8170d5d1cf9aa3e
Eloy <eloyparra.b@gmail.com>
2022-03-16 23:43:08 +0100

    polar plot

--- moon.py
+++ moon.py
@@ -1,3 +1,4 @@
+import numpy as np
 from datetime import datetime
 import ephem
 import matplotlib.pyplot as plt
@@ -23,9 +24,15 @@ for day in calendar.database:
             moon_phases.append(m.moon_phase)
 
 print(moon_phases[-1])
-plt.hist(moon_phases, bins=12)
+n, bins, _ = plt.hist(moon_phases, bins=12)
 plt.xlabel("Moon phase")
 plt.ylabel("Migraine count")
-
 plt.tight_layout()
+
+bin_centers = np.mean(np.array((bins[:-1], bins[1:])), axis=0) * 2 * np.pi
+
+fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
+ax.plot(np.append(bin_centers, bin_centers[0]), np.append(n, [n[0]]), 'o-')
+
+
 plt.show()
