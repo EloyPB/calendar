c85b1a9fdecc384a32383133503017197b98ae32
Eloy <eloyparra.b@gmail.com>
2024-07-21 13:13:26 +0200

    faster

--- musical_migraine.py
+++ musical_migraine.py
@@ -11,21 +11,24 @@ scrobbles_path = '/c/DATA/CLOUD/Documentos/scrobbles.csv'
 
 num_top_artists = 20
 num_time_bins = 12
-num_side_days = 7
+num_side_days = 10
 
 
 scrobbles = pd.read_csv(scrobbles_path)
-scrobbles = scrobbles.loc[:, ('utc_time', 'artist')]
 locale.setlocale(locale.LC_TIME, 'en_US.UTF-8')
 scrobbles.utc_time = pd.to_datetime(scrobbles.utc_time)
 scrobbles = scrobbles[scrobbles.utc_time >= pd.Timestamp('2013-12-01')]
+scrobbles['ord_date'] = scrobbles['utc_time'].dt.date.apply(lambda x: x.toordinal())
+scrobbles = scrobbles.loc[:, ('utc_time', 'ord_date', 'artist')]
+scrobbles = scrobbles.iloc[::-1].reset_index(drop=True)
 
 
 with open(calendar_path, 'r') as file:
     data = json.load(file)
 
-pain_dates = [datetime.strptime(entry['date'], "%Y-%m-%d") for entry in data if entry.get('pain') == True]
-pain_dates = [pain_date for pain_date in pain_dates if pain_date >= scrobbles['utc_time'].iat[-1]]
+pain_dates = [pd.Timestamp(entry['date']) for entry in data if entry.get('pain') == True]
+pain_dates = [pain_date for pain_date in pain_dates if pain_date >= scrobbles['utc_time'].iat[0]]
+pain_ord_dates = [pain_date.toordinal() for pain_date in pain_dates]
 
 artist_counts = scrobbles.artist.value_counts()
 top_artists = artist_counts.head(num_top_artists).index.tolist()
@@ -38,8 +41,10 @@ aligned_totals = np.zeros(num_side_days*2+1)
 for pain_num in range(len(pain_dates) - 1):
     print(f'{pain_num}/{len(pain_dates)}')
     for (offset_num, offset) in enumerate(range(-num_side_days, num_side_days+1)):
-        date = pain_dates[pain_num] + timedelta(days=offset)
-        date_scrobbles = scrobbles[scrobbles['utc_time'].dt.date == date.date()]
+        ord_date = pain_ord_dates[pain_num] + offset
+        first_idx = scrobbles['ord_date'].searchsorted(ord_date, side='left')
+        last_idx = scrobbles['ord_date'].searchsorted(ord_date, side='right') - 1
+        date_scrobbles = scrobbles.iloc[first_idx:last_idx]
         if not date_scrobbles.empty:
             for (artist_num, artist) in enumerate(top_artists):
                 aligned_scrobbles[artist_num, offset+num_side_days] += (date_scrobbles['artist'] == artist).sum()
@@ -48,11 +53,12 @@ for pain_num in range(len(pain_dates) - 1):
             
 aligned_fractions = aligned_scrobbles / aligned_totals
 
-plt.figure()
-for artist_num in range(5):
-    plt.plot(range(-num_side_days, num_side_days+1), aligned_fractions[artist_num, :], label=top_artists[artist_num])
-# plt.axvline(0)
-plt.legend()
+
+for artist_num in range(20):
+    plt.figure()
+    plt.plot(range(-num_side_days, num_side_days+1), aligned_fractions[artist_num, :])
+    plt.axvline(0)
+    plt.title(top_artists[artist_num])
 
         
     
